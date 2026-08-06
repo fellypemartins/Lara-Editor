8be6fb3b78e5b91e2ccc652971667c39f1d8ebd1
fellypemartins <fellypemartins.official@gmail.com>
2026-08-06 19:32:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lara Editor - v0.0.1
+
+```sh
+# How to run Lara
+
+bun install
+bun run dev
+bunx tauri dev
+```
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { version } from "$lib";
+</script>
+
+<h1 class="text-3xl font-semibold text-violet-900 m-10">Lara Editor <sup>v{version}</sup></h1>
+
+<div class="flex">
+    <div class="flex flex-col m-10">
+        <p><span class="text-2xl font-semibold text-violet-900">L</span>eve</p>
+        <p><span class="text-2xl font-semibold text-violet-900">A</span>gil</p>
+        <p>
+            <span class="text-2xl font-semibold text-violet-900">R</span>obusta
+        </p>
+        <p>
+            <span class="text-2xl font-semibold text-violet-900">A</span
+            >cessível
+        </p>
+    </div>
+
+    <div class="flex flex-col m-10">
+        <p><span class="text-2xl font-semibold text-violet-900">L</span>ight</p>
+        <p><span class="text-2xl font-semibold text-violet-900">A</span>gile</p>
+        <p>
+            <span class="text-2xl font-semibold text-violet-900">R</span>obust
+        </p>
+        <p>
+            <span class="text-2xl font-semibold text-violet-900">A</span
+            >ccessible
+        </p>
+    </div>
+</div>
